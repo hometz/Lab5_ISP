48b5d423217c036b1cfe713f530ab7406f422524
hometz <112748014+hometz@users.noreply.github.com>
2024-04-11 10:53:20 +0300

    before_db

---
+++ ProjectManager.Persistence/GlobalUsings.cs
@@ -0,0 +1,14 @@
+﻿global using ProjectManager.Domain.Entities;
+global using ProjectManager.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Persistence
+{
+    internal class GlobalUsings
+    {
+    }
+}
