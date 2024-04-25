6ee445297cdbcb8b852c910d54c46f60ee49b448
hometz <112748014+hometz@users.noreply.github.com>
2024-04-25 13:02:04 +0300

    Repository_relized

---
+++ ProjectManager.Persistence/Repository/EfRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectManager.Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Persistence.Repository
+{
+    public class EfRepository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private DbSet<T> _entities;
+
+        public EfRepository(AppDbContext context)
+        {
+            _context = context;
+            _entities = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _entities.AsEnumerable();
+        }
+
+        public T Get(int id)
+        {
+            return _entities.Find(id);
+        }
+
+        public void Create(T item)
+        {
+            _entities.Add(item);
+            _context.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            T item = _entities.Find(id);
+            if (item != null)
+            {
+                _entities.Remove(item);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
